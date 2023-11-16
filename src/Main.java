329019517bcf954a18dbe06f4c8656fc64cc5346
Konain1 <kaunainkasab@gmail.com>
2023-11-17 00:16:30 +0530

    lec2 java basics

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main{
+
+    static int  $ = 12;
+
+    public static void main(String[] args) {
+
+//        identifier
+
+        int a = 0;
+        int b = 30;
+        int c = 40;
+        int d = 50;
+
+        if(a == c ){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+
+        int num = 11;
+        String result = (num % 2 == 0) ? "E" : "O";
+
+      int res =   (a == 10)  ? 1 : 0;
+
+
+        String r = (a > 0) ? "Positive" : (a < 0) ? "Negative" : "Zero";
+
+
+        System.out.println(r);
+
+
+
+    }
+}
+
+class Car{
+
+}
